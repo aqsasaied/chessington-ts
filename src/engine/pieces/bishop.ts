a1b6a3807406525deb7400a32225d6db094048f8
Bianca <bianca.darolti@softwire.com>
2022-09-30 12:29:36 +0100

    bishop working

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,9 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
+
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +11,44 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var availableMoves:Square[] = [];
+        availableMoves = availableMoves.concat(this.checkMoves(board, true, true))
+        availableMoves = availableMoves.concat(this.checkMoves(board, false, true))
+        availableMoves = availableMoves.concat(this.checkMoves(board, true, false))
+        availableMoves = availableMoves.concat(this.checkMoves(board, false, false))
+        console.log(availableMoves);
+        return availableMoves;
+
+    }
+
+    public checkMoves(board: Board, up:Boolean, right:Boolean){
+        var moves:Square[] = [];
+        var newLocation:Square = board.findPiece(this);
+        var end:number
+        while (newLocation.row > 0 && newLocation.col > 0 && newLocation.row < GameSettings.BOARD_SIZE - 1 && newLocation.col < GameSettings.BOARD_SIZE - 1){
+            if (up){
+                if (right){
+                    newLocation = Square.at(newLocation.row + 1, newLocation.col + 1);
+                }
+                else{
+                    newLocation = Square.at(newLocation.row + 1, newLocation.col - 1);
+                }
+            }
+            else{
+                if (right){
+                    newLocation = Square.at(newLocation.row - 1, newLocation.col + 1);
+                }
+                else{
+                    newLocation = Square.at(newLocation.row - 1, newLocation.col - 1);
+                }
+            }
+            if (board.getPiece(newLocation) == undefined){
+                moves.push(newLocation);
+            }
+            else{
+                break;
+            }
+        }
+        return (moves);
     }
 }
