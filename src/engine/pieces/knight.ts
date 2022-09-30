4ca99e63a52c5b5e2a43fc063b651a1babc1819a
Bianca <bianca.darolti@softwire.com>
2022-09-30 15:04:04 +0100

    knight working

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,14 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var possibleMoves = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[-2,1],[-2,-1],[2,-1]];
+        var newLocation:Square = board.findPiece(this);
+        var availableMoves:Square[] = [];
+        for (var move of possibleMoves){
+            if (board.isOnBoard(Square.at(newLocation.row + move[0], newLocation.col + move[1]))){
+                availableMoves.push(Square.at(newLocation.row + move[0], newLocation.col + move[1]))
+            }
+        }         
+        return availableMoves;
     }
 }
