65cca45fe07ef607a5236ee40a3dd4caa3405036
Aqsa Saied <Aqsa.Saied@softwire.com>
2022-10-25 10:17:33 +0100

    all pieces can take

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -15,6 +15,17 @@ export default class Pawn extends Piece {
         if (this.numMoves == 0 && availableMoves.length > 0){
             availableMoves = availableMoves.concat(this.singleMove(board, [[increment*2,0]]));
         }
+        var currentSquare = board.findPiece(this);
+        var d1 = Square.at(currentSquare.row + increment, currentSquare.col + 1);
+        var d2 = Square.at(currentSquare.row + increment, currentSquare.col - 1);
+        if (board.isOnBoard(d1) && !board.isEmpty(d1) && board.isTakeable(d1)){
+            availableMoves.push(d1);
+        }
+        if (board.isOnBoard(d2) && !board.isEmpty(d2) && board.isTakeable(d2)){
+            availableMoves.push(d2);
+        }
+        console.log(availableMoves);
         return availableMoves;
     }
+
 }
